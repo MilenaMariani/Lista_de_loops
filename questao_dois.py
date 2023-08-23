94849b1a3ecce36c01be0ab882df3168d9ec27a2
MilenaMariani <142273910+MilenaMariani@users.noreply.github.com>
2023-08-23 19:55:10 -0300

    atividade_loops

---
+++ questao_dois.py
@@ -0,0 +1,10 @@
+i = 1
+
+soma = 0
+
+while i <= 100:
+    soma += i 
+
+    i += 1
+
+print(soma)
---
+++ questao_tres.py
@@ -0,0 +1,10 @@
+i = 1
+
+tabuada = 0
+numero_usuario = int (input("Digite um numero : "))
+
+while i <= 10:
+    tabuada = (i * numero_usuario)    
+    print (tabuada)
+
+    i +=1
