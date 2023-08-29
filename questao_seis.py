daf719aec93a2023ae13cd413aee567df4d2ce96
MilenaMariani <142273910+MilenaMariani@users.noreply.github.com>
2023-08-28 21:07:49 -0300

    atualização.2

---
+++ questao_seis.py
@@ -0,0 +1,10 @@
+num = int (input ("Quantos numeros deseja contar?: "))
+n1 = 0
+n2 = 1
+print (n1)
+print (n2)
+
+for x in range (1, n-1):
+    resposta = n1 + n2 
+    print (resposta)
+    n1 = n2n2 = resposta
---
+++ questao_sete.py
@@ -0,0 +1,7 @@
+c = 0
+num = int (input("Qual numero voce deseja tirar o fatorial?: "))
+dim -=s
+while num < c :
+    num -=s
+
+    c +=1
