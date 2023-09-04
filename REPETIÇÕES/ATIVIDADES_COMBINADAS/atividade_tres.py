1ed0a25f01d5689fec9fd1dee4940f8f2717d77b
MilenaMariani <142273910+MilenaMariani@users.noreply.github.com>
2023-09-04 20:47:48 -0300

    atualização.3

--- REPETIÇÕES/ATIVIDADES_COMBINADAS/atividade_tres.py
+++ REPETIÇÕES/ATIVIDADES_COMBINADAS/atividade_tres.py
@@ -1,2 +1,17 @@
+import random
 
+numero_computador = random.randint(1, 10)
 
+tentativas = 3
+
+while tentativas > 0:
+    print(f"Você tem {tentativas} tentativa(s).")
+    resp_usuario = int(input("Tente adivinhar o número: "))
+
+    if resp_usuario == numero_computador:
+        print("Parabéns! Você acertou!")
+        break
+    else:
+        tentativas -= 1
+
+print(f"O número era: {numero_computador}!")
